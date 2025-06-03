2aaebc729990e10f2687ef7438879b3eb0bd7830
paoladera <paolarladera@gmail.com>
2025-06-03 20:54:31 -0300

    Fix: arreglo el error del app module

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,13 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-    // Habilitar CORS
+  // Habilitar CORS
   app.enableCors({
     origin: 'http://localhost:3001', // O el puerto donde corre tu Next.js
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
