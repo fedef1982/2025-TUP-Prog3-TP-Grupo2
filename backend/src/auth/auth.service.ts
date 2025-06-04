1ede29602364ea9c607642208e7475c63a693db5
paoladera <paolarladera@gmail.com>
2025-06-03 21:28:03 -0300

    Feat: Borro la implementación del Passport

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/usuario/usuario.service';
 import * as bcrypt from 'bcrypt';
@@ -11,16 +11,20 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
-  async validateUser(email: string, pass: string): Promise<User | null> {
+  async validateUser(email: string, pass: string): Promise<User> {
     const user = await this.usersService.findByEmail(email);
-    if (user && (await bcrypt.compare(pass, user.contrasenia))) {
-      return user;
+
+    if (!user || !(await bcrypt.compare(pass, user.contrasenia))) {
+      throw new UnauthorizedException('Credenciales inválidas');
     }
-    return null;
+
+    return user;
   }
 
-  async login(user: User): Promise<{ access_token: string }> {
-    const payload = { sub: user.id, rol: user.rol };
+  async signIn(email: string, pass: string): Promise<{ access_token: string }> {
+    const user = await this.validateUser(email, pass);
+
+    const payload = { sub: user.id, username: user.email, rol: user.rol };
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
