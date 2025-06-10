d9db01d3d86ba45dbb29f199024f78938dd84892
AdrielReina <adriel.a.reina@gmail.com>
2025-06-10 20:44:33 -0300

    swagger basic dependencies

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Adoptar example')
+    .setDescription('The AdoptAR API description')
+    .setVersion('1.0')
+    .addTag('tagDePrueba')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   // Habilitar CORS
   app.enableCors({
