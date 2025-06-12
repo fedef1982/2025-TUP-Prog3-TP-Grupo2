ac678a0ef7df76ff4538dabd369798b435785205
AdrielReina <adriel.a.reina@gmail.com>
2025-06-12 00:08:40 -0300

    Update login.dto.ts

--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -1,4 +1,5 @@
 import { IsEmail, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
   @ApiProperty({ example: 'contacto@dominio.com' })
