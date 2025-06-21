27d8d7dd1edc233f9158963b5fc29ca485ee594f
marcosvp5 <marcosvelardez40@gmail.com>
2025-06-21 20:44:12 -0300

    grabando 'especie.controller.spec.ts'

---
+++ backend/test/mascota_test/especie_test/especie.controller.spec.ts
@@ -0,0 +1,42 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { EspecieController } from '../../../src/mascota/especie/especie.controller';
+import { EspecieService } from '../../../src/mascota/especie/especie.service';
+import { Especie } from '../../../src/mascota/especie/especie.model';
+
+describe('EspecieController', () => {
+  let especieController: EspecieController;
+  let especieService: EspecieService;
+
+  beforeEach(async () => {
+    const mockEspecieService = {
+      findAll: jest.fn(),
+    };
+
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      controllers: [EspecieController],
+      providers: [
+        {
+          provide: EspecieService,
+          useValue: mockEspecieService,
+        },
+      ],
+    }).compile();
+
+    especieService = moduleRef.get<EspecieService>(EspecieService);
+    especieController = moduleRef.get<EspecieController>(EspecieController);
+  });
+
+  describe('findAll', () => {
+    it('debería retornar un arreglo de especies', async () => {
+      const result: Especie[] = [
+        { id: 1, nombre: 'Especie 1' } as Especie,
+        { id: 2, nombre: 'Especie 2' } as Especie,
+      ];
+
+      jest.spyOn(especieService, 'findAll').mockResolvedValue(result);
+
+      expect(await especieController.findAll()).toBe(result);
+      expect(especieService.findAll).toHaveBeenCalled();
+    });
+  });
+});
